0e27642e300787d30b3eddee61baeb95c5ce9b01
Petr Pucil <petr.pucil@seznam.cz>
2025-01-26 12:12:00 +0100

    Improve extract-strings-from-elfs.py

--- ubuntu-packages/extract-strings-from-elfs.py
+++ ubuntu-packages/extract-strings-from-elfs.py
@@ -14,9 +14,9 @@ out_dir.mkdir(exist_ok=True)
 
 # https://github.com/armijnhemel/binaryanalysis-ng/blob/e05071e01213c7d7d7261e979ab1d308872e87d0/src/bang/parsers/executable/elf/UnpackParser.py#L62-L65
 # Read only data sections. This should be expanded.
-RODATA_SECTIONS = ['.rodata', '.rodata.str1.1', '.rodata.str1.4',
+RODATA_SECTIONS = ('.rodata', '.rodata.str1.1', '.rodata.str1.4',
                    '.rodata.str1.8', '.rodata.cst4', '.rodata.cst8',
-                   '.rodata.cst16', 'rodata']
+                   '.rodata.cst16', 'rodata')
 
 STRING_SEPARATOR_REGEX = re.compile(r'[\x00-\x08\x0a-\x1f\x7f\ufffd]+')
 
@@ -46,20 +46,24 @@ def extract_strings_from_elf(elf_path: Path | str) -> dict:
 
             # https://github.com/armijnhemel/binaryanalysis-ng/blob/e05071e01213c7d7d7261e979ab1d308872e87d0/src/bang/parsers/executable/elf/UnpackParser.py#L774-L795
             for s in body.split(b'\x00'):
-                decoded_s = s.decode('utf-8', errors='replace')
-                # We look for the last UTF-8 decode error, which is indicated by the
-                # U+FFFD REPLACEMENT CHARACTER. If we find it, we only consider the part
-                # after it until the b'\x00' byte and ignore everything before it. The
-                # logic is that strings in C are null-terminated, so any actual string
-                # literal in C source code will end with b'\x00', but it can start
-                # anywhere in the .rodata section right after any "garbage" (some generic
-                # read-only data not coming from a string literal). If we are lucky, this
-                # garbage will fail to decode to UTF-8 somewhere, in which case we can
-                # limit the range where we look for strings, which will filter out the
-                # nonsense strings found in the binary garbage from the output.
-                repl_ch_idx = decoded_s.rfind('\ufffd')
-                if repl_ch_idx != -1:
+                try:
+                    decoded_s = s.decode('utf-8')
+                except UnicodeDecodeError:
+                    decoded_s = s.decode('utf-8', errors='replace')
+                    # We look for the last UTF-8 decode error, which is indicated by the
+                    # U+FFFD REPLACEMENT CHARACTER. If we find it, we only consider the part
+                    # after it until the b'\x00' byte and ignore everything before it. The
+                    # logic is that strings in C are null-terminated, so any actual string
+                    # literal in C source code will end with b'\x00', but it can start
+                    # anywhere in the .rodata section right after any "garbage" (some generic
+                    # read-only data not coming from a string literal). If we are lucky, this
+                    # garbage will fail to decode to UTF-8 somewhere, in which case we can
+                    # limit the range where we look for strings, which will filter out the
+                    # nonsense strings found in the binary garbage from the output.
+                    repl_ch_idx = decoded_s.rfind('\ufffd')
+                    assert repl_ch_idx != -1
                     decoded_s = decoded_s[repl_ch_idx + 1:]
+
                 decoded_strings = STRING_SEPARATOR_REGEX.split(decoded_s)
                 for decoded_string in decoded_strings:
                     if len(decoded_string) < STRING_CUTOFF_LENGTH:
@@ -82,18 +86,19 @@ def extract_strings_from_elf(elf_path: Path | str) -> dict:
 
             # https://github.com/armijnhemel/binaryanalysis-ng/blob/e05071e01213c7d7d7261e979ab1d308872e87d0/src/bang/parsers/executable/elf/UnpackParser.py#L676-L687
             for entry in entries:
-                symbol_name = entry.name or ''
                 if entry.bind != Elf.SymbolBinding.global_symbol:
-                    # print(f'Skipping symbol {symbol_name!r} because it has binding {entry.bind!r}')
+                    # print(f'Skipping symbol {entry.name!r} because it has binding {entry.bind!r}')
                     continue
+
+                symbol_name = entry.name
+                assert symbol_name is not None
 
                 if entry.type == Elf.SymbolType.func:
                     if entry.sh_idx_special == Elf.SectionHeaderIdxSpecial.undefined:
                         undefined_functions.append(symbol_name)
                     else:
                         defined_functions.append(symbol_name)
-
-                if entry.type == Elf.SymbolType.object:
+                elif entry.type == Elf.SymbolType.object:
                     if entry.sh_idx_special == Elf.SectionHeaderIdxSpecial.undefined:
                         undefined_objects.append(symbol_name)
                     else:
@@ -114,7 +119,9 @@ def extract_strings_from_blob(path: Path) -> list[str]:
 def main():
     json_from_elfs = {}
     json_from_blobs = {}
-    for elf_path in tqdm(sorted(elfs_dir.glob('**/*'))):
+    elfs = [path for path in elfs_dir.glob('**/*') if path.is_file()]
+    elfs.sort()
+    for elf_path in tqdm(elfs):
         if not elf_path.is_file():
             continue
         rel_elf_path = str(elf_path.relative_to(elfs_dir))
